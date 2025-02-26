9664d3554eb3b1a184ce61a855b01497f3a4a2f1
Luca Fourfooz <lucacist467@gmail.com>
2025-02-26 02:40:40 +0100

    deploi3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -25,7 +25,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router basename="/new-life-v1">
+      <Router>
         <div className="app">
           <Navbar />
           <Routes>
